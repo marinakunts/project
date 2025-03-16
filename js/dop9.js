6750553f9cc84ef0c0ebd71fa8b20501f4681a06
Marina <example.com>
2025-03-16 20:08:09 +0100

    First commit

---
+++ js/dop9.js
@@ -0,0 +1,15 @@
+const vehicleBodyWidth = 5000;
+const vehicleBodyLenth = 4000;
+
+console.log();
+
+// //Request
+// data;
+// response;
+
+// cosnt
+// snake_case
+// UPPER_SNAKE_CASE
+let COLOR_RED = "#F00";
+// kebab-case
+// PascalCase - nazwaniej klasow
---
+++ js/script1.js
@@ -0,0 +1,146 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1; //camelCase
+
+// number = 10;
+// console.log(number);
+
+// //Exercise 1:
+// let userName = "John";
+// let userNumber = 25;
+// userNumber = 24;
+
+// const obj = {
+//   a: 50,
+// };
+// obj.a = 10;
+// console.log(obj);
+
+// //Hosting
+// console.log(name);
+// var name = "Ivan"; //var = let
+
+// // //
+// {
+//   let result = 50;
+// }
+
+// console.log(result);
+
+//10
+// let number = 4.6;
+
+// console.log(-4 / 0);
+// console.log("string" * 9);
+
+// const persone = "Alex lorem5";
+
+// const bool = true;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+// //Objects
+// const obj = {
+//   name: "John",
+//   age: 25,
+//   isMarried: false,
+// };
+
+// // console.log(obj.name)
+// console.log(obj['name']);
+
+// //Array
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 6, {}, []];
+// console.log(arr[6])
+
+//11
+// const arr = [1, 2, 3];
+
+// arr[10] = "3456";
+
+// console.log(arr);
+// const obj = { a: 1, b: 2 };
+
+// const arrObj = {
+//   a: "a",
+//   1: "b",
+//   2: "c",
+//   abc: {
+//     df: [{}, {}],
+//     def: {},
+//   },
+// };
+// const b = "b";
+
+// arrObj[b] = '1234'
+// arrObj.b = "1234";
+// !!! arrObj['b'] = "1234";
+
+// console.log(arrObj.b);
+
+//Exercise 2
+// let storeName = "Shop";
+// const storeDescription = {
+//   budget: 10000,
+//   employees: ["Anna", "Alex", "Anton"],
+//   products: {
+//     note: 200,
+//     book: 300,
+//   },
+
+//   open: true,
+// };
+
+//12
+
+// alert("Hello");
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Are you 18 yo?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = prompt("What is your name?", "");
+// answers[1] = prompt("What is your lastname?", "");
+// answers[2] = prompt("How old are you?", "");
+
+// console.log(answers);
+// console.log(typeof null);
+
+// let arr = [1, 2, 3];
+// console.log(typeof arr); // "object"
+
+//13
+// const category = "toys";
+
+// console.log(`https://someurl.com/${category}`);
+
+// const user = "name";
+
+// alert(`Privet, ${user}`);
+
+//14
+// console.log(4 + +"5");
+
+// let incr = 10,
+//   decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+console.log(5 % 2);
+
+console.log(2 + 2 * 2 !== "6");
+
+const isChecked = false,
+  isCLose = false;
+
+console.log(isChecked || !isCLose);
